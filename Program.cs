e677af5a5e187d30ee3a769e6cd8c0e1518f8a8b
marcelo.evaristo <marcelo.evaristo@5LT37Z2>
2023-11-13 09:34:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using TransformarDateTime;
+
+Valores valores = new Valores();
+DateTime current = DateTime.Now;
+int anoMesValor = valores.TransformData(current);
+Console.WriteLine($"Data atual: {anoMesValor}");
+
---
+++ Valores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransformarDateTime
+{
+    public class Valores
+    {
+        public int TransformData(DateTime data)
+        {
+            return int.Parse(data.ToString("yyyyMM"));
+        }
+    }
+}
